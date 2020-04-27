e901781d91d1d4e849c408083008073a905e647b
ve2 <ve2@ve2deMacBook-Air.local>
2020-04-27 14:04:17 +0800

    A 完成功能1

---
+++ Branch_test2/AViewController.h
@@ -0,0 +1,17 @@
+//
+//  AViewController.h
+//  Branch_test2
+//
+//  Created by ve2 on 2020/4/27.
+//  Copyright © 2020 ve2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
